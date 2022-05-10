2602fec3ccdb48098be2501bee414c6438cfb34e
Rafael <perezaguirrerafael@gmail.com>
2022-05-09 23:29:36 -0500

    change view to class

--- app/home/views.py
+++ app/home/views.py
@@ -1,7 +1,15 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import View
 
 # Create your views here.
 def index(request):
     context = {'segment': 'index'}
     return render(request, 'home/index.html', context)
+
+
+class Dashboard(LoginRequiredMixin, View):
+    def get(self, request):
+        context = {'segment': 'index'}
+        return render(request, 'home/index.html', context)

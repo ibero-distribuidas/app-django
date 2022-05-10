0407dddd0991f6e2534cdfed936eee63a87a3f29
Rafael <perezaguirrerafael@gmail.com>
2022-05-09 21:15:52 -0500

    add template an static files

--- app/home/views.py
+++ app/home/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Bienvenido")
+    context = {'segment': 'index'}
+    return render(request, 'home/index.html', context)

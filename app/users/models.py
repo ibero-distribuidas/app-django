fc3832053c3161a34069fdd7f0d089d477d852c2
Rafael <perezaguirrerafael@gmail.com>
2022-05-09 20:52:16 -0500

    fix user table

--- app/users/models.py
+++ app/users/models.py
@@ -1,13 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class User(models.Model):
-    name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=50)
-    email = models.EmailField()
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     phone = models.CharField(max_length=10)
-    created = models.DateTimeField(auto_now_add=True)
     rfc = models.CharField(max_length=15, blank=True)
+    bithday = models.DateField(blank=True)
+    sexo = models.CharField(max_length=1)
 
     def __str__(self):
         return self.name
